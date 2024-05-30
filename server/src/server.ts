b6a69291e5ad2774a395c6ecf6912856a95d83b2
Somyajeet <isomya13@gmail.com>
2024-05-30 22:29:08 +0530

    chore: setup apollo server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,21 @@
 import mongoose from 'mongoose';
 import serverConfig from './config/serverConfig';
+import { ApolloServer } from 'apollo-server';
+import typeDefs from './typedefs';
+import resolvers from './resolvers';
+
+
+const server = new ApolloServer({
+    typeDefs: typeDefs,
+    resolvers: resolvers
+})
 
 async function start(){
 
     await mongoose.connect(serverConfig.MONGODB_URI)
-    console.log("connected to mongodb")
+    console.log("connected to mongodb..")
+    const {url} = await server.listen();
+    console.log(`graphql server is up too on ${url}`);
 }
 
 
